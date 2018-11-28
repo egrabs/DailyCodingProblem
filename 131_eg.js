e3e61a2d58ba747682ce00a81d2f064428058e3d
Elyes Graba <elyesgraba@gatech.edu>
2018-11-27 18:25:12 -0600

    remove unicode chars

--- 131_eg.js
+++ 131_eg.js
@@ -1,4 +1,4 @@
-// Given the head to a singly linked list, where each node also has a “random” pointer that points to anywhere
+// Given the head to a singly linked list, where each node also has a "random" pointer that points to anywhere
 // in the linked list, deep clone the list.
 
 const assert = require('assert');
