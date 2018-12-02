ef86212db0daceb3091eb647c2324384c8b68e19
Elyes Graba <elyesgraba@gatech.edu>
2018-12-01 18:01:09 -0600

    go back and improve reverse ll problem

--- 73_eg.py
+++ 73_eg.py
@@ -1,5 +1,8 @@
 # Given the head of a singly linked list, reverse it in-place.
 
+# 1 -> 2 -> 3 -> 4
+# 4 -> 3 -> 2 -> 1
+
 # I know there's builtin stacks in python
 # and I know even python lists have a .pop() method
 # but whatever, why not jusss do it
@@ -21,12 +24,20 @@ class Stack:
         return len(self.stack) == 0
 
 class Node:
-    def __init__(self, val):
+    def __init__(self, val, nextNode=None):
         self.val = val
-        self.next = None
+        self.next = nextNode
 
-def reverseLinkedList(head):
-    stk = new Stack()
+def printLL(head):
+    vals = []
+    node = head
+    while node is not None:
+        vals.append(str(node.val))
+        node = node.next
+    print '->'.join(vals)
+
+def reverseLinkedListWithStack(head):
+    stk = Stack()
     node = head
     while (node is not None):
         stk.push(node)
@@ -40,3 +51,25 @@ def reverseLinkedList(head):
 
     node.next = None
     return newHead
+
+def reverseLinkedListInPlace(head):
+    prev = head
+    node = head.next
+    prev.next = None
+    while node is not None:
+        temp = node.next
+        node.next = prev
+        prev = node
+        node = temp
+    return prev
+
+def buildLL():
+    four = Node(4)
+    three = Node(3, four)
+    two = Node(2, three)
+    one = Node(1, two)
+    return one
+
+printLL(reverseLinkedListWithStack(buildLL()))
+printLL(reverseLinkedListInPlace(buildLL()))
+
