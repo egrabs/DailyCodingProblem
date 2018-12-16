412c6c0253eb3c466f088d0282935326665283a1
Elyes Graba <egrabs@Elyess-MacBook-Pro.local>
2018-12-16 11:12:33 -0600

    fix a bug in finding the shortest substring problem

--- elyes/103_eg.js
+++ elyes/103_eg.js
@@ -2,6 +2,31 @@
 
 // For example, given the string "figehaeci" and the set of characters {a, e, i}, you should return "aeci".
 
+// this is wrong :(
+// function shortestSubstring(str, charSet) {
+//     function covers(substr) {
+//         // trying to force myself to start using const in loops
+//         // but it feels so wrong
+//         for (const char of charSet) {
+//             if (!substr.includes(char))
+//                 return false;
+//         }
+//         return true;
+//     }
+//     let start = 0;
+//     let end = str.length;
+//     let substr = str.slice(start, end);
+//     while (covers(substr)) {
+//         substr = str.slice(++start, end);
+//     }
+//     substr = str.slice(--start, end);
+//     while (covers(substr)) {
+//         substr = str.slice(start, --end);
+//     }
+//     end++;
+//     return str.slice(start, end);
+// }
+
 function shortestSubstring(str, charSet) {
     function covers(substr) {
         // trying to force myself to start using const in loops
@@ -12,20 +37,24 @@ function shortestSubstring(str, charSet) {
         }
         return true;
     }
-    let start = 0;
-    let end = str.length;
-    let substr = str.slice(start, end);
-    while (covers(substr)) {
-        substr = str.slice(++start, end);
+
+    let minLen = Infinity;
+    let minSlice;
+    for (let i=0; i<str.length-1; i++) {
+        for (let j=i+charSet.size; j<=str.length; j++) {
+            let substr = str.slice(i, j);
+            if (covers(substr)) {
+                if ((j-i) < minLen) {
+                    minLen = j-i;
+                    minSlice = substr;
+                }
+            }
+        }
     }
-    substr = str.slice(--start, end);
-    while (covers(substr)) {
-        substr = str.slice(start, --end);
-    }
-    end++;
-    return str.slice(start, end);
+    return minSlice;
 }
 
 console.log(shortestSubstring('figehaeci', new Set(['a', 'e', 'i'])));
 console.log(shortestSubstring('figehaei', new Set(['a', 'e', 'i'])));
 console.log(shortestSubstring('figehaeciffrtq', new Set(['a', 'e', 'i'])));
+console.log(shortestSubstring('aecifdsahjklhfiacci', new Set(['a', 'e', 'i'])));
